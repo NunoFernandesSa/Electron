41afad580175c49f9c170aab0f02ed9d655b9f7b
NunoF <n.fernandes.contact@gmail.com>
2025-08-21 06:34:16 +0200

    feat: add preload script and update main window configuration

--- src/main/main.ts
+++ src/main/main.ts
@@ -2,9 +2,14 @@ import { app, BrowserWindow } from "electron";
 import * as path from "path";
 import { pollResources } from "./resourceManager.js";
 import { isDev } from "./utils/check-if-is-dev.js";
+import { getPreloadPath } from "./pathResolver.js";
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+  });
 
   if (isDev()) {
     mainWindow.loadURL("http://localhost:5123");
